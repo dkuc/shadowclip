e979f8ca75a7488a94c3d7f3d7d5b6a34c941f51
Chris Kyrouac <chriskyrouac@gmail.com>
2017-04-09 09:31:07 -0400

    detect awp shots via audio poc

--- ShadowClip/GUI/VideoViewModel.cs
+++ ShadowClip/GUI/VideoViewModel.cs
@@ -9,15 +9,31 @@ using System.Windows.Threading;
 using Caliburn.Micro;
 using ShadowClip.GUI.UploadDialog;
 using ShadowClip.services;
+using System.Threading;
+using System.Threading.Tasks;
 
 using OpenCvSharp;
 using OpenCvSharp.ML;
 using System.Windows.Media.Imaging;
 
+using Tesseract;
+using SoundFingerprinting.Builder;
+using SoundFingerprinting.Audio;
+using SoundFingerprinting;
+using SoundFingerprinting.Audio.NAudio;
+using SoundFingerprinting.InMemory;
+using SoundFingerprinting.DAO.Data;
+using SoundFingerprinting.Query;
+
 namespace ShadowClip.GUI
 {
     public sealed class VideoViewModel : Screen, IHandle<FileSelected>
     {
+        private readonly IModelService modelService = new InMemoryModelService();
+        private readonly IAudioService audioService = new NAudioService();
+        private readonly IFingerprintCommandBuilder fingerprintCommandBuilder = new FingerprintCommandBuilder();
+        private readonly IQueryCommandBuilder queryCommandBuilder = new QueryCommandBuilder();
+
         private readonly IDialogBuilder _dialogBuilder;
         private readonly IShotFinder _shotFinder;
         private readonly TimeSpan _frameTime = TimeSpan.FromTicks(166667);
@@ -27,6 +43,44 @@ namespace ShadowClip.GUI
         private VideoView _videoView;
 
         public VideoViewModel(IEventAggregator eventAggregator, ISettings settings, IDialogBuilder dialogBuilder, IShotFinder shotFinder)
+
+        public void StoreAudioFileFingerprintsInStorageForLaterRetrieval(string pathToAudioFile)
+        {
+            var track = new TrackData("1", "test-artist", "test-song", "test-album", 2017, 290);
+
+            // store track metadata in the datasource
+            var trackReference = modelService.InsertTrack(track);
+
+            // create hashed fingerprints
+            var hashedFingerprints = fingerprintCommandBuilder
+                                        .BuildFingerprintCommand()
+                                        .From(pathToAudioFile)
+                                        .UsingServices(audioService)
+                                        .Hash()
+                                        .Result;
+
+            // store hashes in the database for later retrieval
+            modelService.InsertHashDataForTrack(hashedFingerprints, trackReference);
+        }
+
+        public QueryResult GetBestMatchForSong(string queryAudioFile)
+        {
+            //int secondsToAnalyze = 4; // number of seconds to analyze from query file
+            //int startAtSecond = 0; // start at the begining
+
+            // query the underlying database for similar audio sub-fingerprints
+                                                 //.From(queryAudioFile, secondsToAnalyze, startAtSecond)
+            var queryResult = queryCommandBuilder.BuildQueryCommand()
+                                                 .From(queryAudioFile, 6, 8)
+                                                 .UsingServices(modelService, audioService)
+                                                 .Query()
+                                                 .Result;
+
+            return queryResult;
+            // return queryResult.BestMatch.Track; // successful match has been found
+        }
+
+        public VideoViewModel(IEventAggregator eventAggregator, ISettings settings, IDialogBuilder dialogBuilder)
         {
             _settings = settings;
             _dialogBuilder = dialogBuilder;
@@ -45,6 +99,8 @@ namespace ShadowClip.GUI
         public bool IsFindingShots { get; set; }
 
         public string IsScopeFrame { get; set; } = "false";
+
+        public string WordString { get; set; } = "";
 
         public MediaElement VideoPlayer => _videoView.Video;
 
@@ -254,8 +310,9 @@ namespace ShadowClip.GUI
         public void Screenshot()
         {
             var screenShot = VideoPlayer.GetScreenShot(Zoom);
-
             Clipboard.SetImage(screenShot);
+
+            //scope detection
             using (var fileStream = new FileStream("temp.png", FileMode.Create))
             {
                 BitmapEncoder encoder = new PngBitmapEncoder();
@@ -285,6 +342,78 @@ namespace ShadowClip.GUI
             {
                 IsScopeFrame = "false";
             }
+
+
+            //text recognition
+            var engine = new TesseractEngine(@"C:/Users/chris/Downloads/tessdata", "eng", EngineMode.Default);
+            var img = Pix.LoadFromFile("temp.png");
+            var page = engine.Process(img);
+            var text = page.GetText();
+            using (var iter = page.GetIterator())
+            {
+                iter.Begin();
+
+                do
+                {
+                    do
+                    {
+                        do
+                        {
+                            do
+                            {
+                                if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                                {
+                                    Console.WriteLine("<BLOCK>");
+                                }
+
+                                var word = iter.GetText(PageIteratorLevel.Word);
+                                WordString = WordString + word + " ";
+                                Console.Write(word);
+                                Console.Write(" ");
+
+                                if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                                {
+                                    Console.WriteLine();
+                                }
+                            } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                            if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                            {
+                                Console.WriteLine();
+                            }
+                        } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                    } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                } while (iter.Next(PageIteratorLevel.Block));
+            }
+
+            //audio location
+            try
+            {
+                string videoFile = "C:/Users/Chris/Videos/Counter-strike  Global Offensive/Counter-strike  Global Offensive 04.03.2017 - 22.15.01.26.DVR.mp4";
+                var process = new System.Diagnostics.Process
+                {
+                    StartInfo =
+                    {
+                        RedirectStandardError = true,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        FileName = @"ffmpeg.exe",
+                        Arguments =
+                            $"-nostdin -i \"{videoFile}\" -vn -ar 44100 -ac 2 -ab 192k -f mp3 \"temp.mp3\""
+                    }
+                };
+
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+
+            StoreAudioFileFingerprintsInStorageForLaterRetrieval("C:/Users/Chris/Downloads/awp_sounds/awp/awp_01.wav");
+            QueryResult result = GetBestMatchForSong("temp.mp3"); //2, 11, 20
 
             using (new OpenCvSharp.Window("Circles", dst))
             {
